be25687fcd928f7874318560707efbcdb8364600
number000000 <maggieliulmh@gmail.com>
2023-04-20 21:31:40 -0700

    cleared out unnecessary debugging components

--- _fcn/qp_DeeP_LCC.py
+++ _fcn/qp_DeeP_LCC.py
@@ -1,27 +1,27 @@
 import numpy as np
 from cvxopt import solvers
-import scipy.io as spio
 from cvxopt import matrix
-import pandas as pd
+#import scipy.io as spio  #For debugging
 
-mat = spio.loadmat('data.mat', squeeze_me=True)
+# For debugging
+# mat = spio.loadmat('data.mat', squeeze_me=True)
+# Up = np.matrix(mat['Up']);
+# Yp = np.matrix(mat['Yp']);
+# Uf = np.matrix(mat['Uf']);
+# Yf = np.matrix(mat['Yf']);
+# Ep = np.matrix(mat['Ep']);
+# Ef = np.matrix(mat['Ef']);
+# uini = np.matrix(mat['uini']);
+# yini = np.matrix(mat['yini']);
+# eini = np.matrix(mat['eini']);
+# Q = np.matrix(mat['Q']);
+# R = np.matrix(mat['R']);
+# r = np.matrix(mat['r']);
+# lambda_g = np.matrix(mat['lambda_g']);
+# lambda_y = np.matrix(mat['lambda_y']);
+# u_limit = np.matrix(mat['u_limit']);
+# s_limit = np.matrix(mat['s_limit']);
 
-Up = np.matrix(mat['Up']);
-Yp = np.matrix(mat['Yp']);
-Uf = np.matrix(mat['Uf']);
-Yf = np.matrix(mat['Yf']);
-Ep = np.matrix(mat['Ep']);
-Ef = np.matrix(mat['Ef']);
-uini = np.matrix(mat['uini']);
-yini = np.matrix(mat['yini']);
-eini = np.matrix(mat['eini']);
-Q = np.matrix(mat['Q']);
-R = np.matrix(mat['R']);
-r = np.matrix(mat['r']);
-lambda_g = np.matrix(mat['lambda_g']);
-lambda_y = np.matrix(mat['lambda_y']);
-u_limit = np.matrix(mat['u_limit']);
-s_limit = np.matrix(mat['s_limit']);
 # =========================================================================
 #                   DeeP-LCC for mixed traffic
 # Input:
@@ -84,17 +84,12 @@ def qp_DeeP_LCC(*args):
     # ------------
     m        = uini.shape[0];                # dimension of control input
     p        = yini.shape[0];                # dimension of output
-    Tini     = Up.shape[0] // m;                # horizon of past data
-    N        = Uf.shape[0] // m;                # horizon of future data
+    Tini     = Up.shape[0] // m;             # horizon of past data
+    N        = Uf.shape[0] // m;             # horizon of future data
     T        = Up.shape[1] + Tini + N - 1;   # time length of pre-collected data
 
     # reshape past data into one single trajectory
     # uini = col(u(-Tini),u(-Tini+1),...,u(-1)) (similarly for yini and eini)
-    #print(yini);
-    #uini_col = uini.reshape(m * Tini, 1);
-    #yini_col = yini.reshape(p * Tini, 1);
-    #eini_col = eini.reshape(Tini, 1);
-    #r_col    = r.reshape(p * N, 1);
     uini_col = np.reshape(uini, (m * Tini, 1), order="F");
     yini_col = np.reshape(yini, (p * Tini, 1), order="F");
     eini_col = np.reshape(eini, (Tini, 1), order="F");
@@ -107,6 +102,7 @@ def qp_DeeP_LCC(*args):
         Q_blk[((i-1)*p+1)-1 : i*p, ((i-1)*p+1)-1 : i*p] = Q;
         R_blk[((i-1)*m+1)-1 : i*m, ((i-1)*m+1)-1 : i*m] = R; 
 
+
     # ---------------------
     # cvxopt  - QP solver in Python
     # from cvxopt import solvers
@@ -117,12 +113,10 @@ def qp_DeeP_LCC(*args):
     # ---------------------
 
     # Coefficient
-    #P = np.dot(Yf.transpose(), Q_blk, Yf) + np.dot(Uf.transpose(), R_blk, Uf) + np.dot(lambda_g, np.eye(T-Tini-N+1)) + np.dot(lambda_y, Yp.transpose(), Yp);
     P = np.dot(np.dot(Yf.transpose(), Q_blk), Yf);
     P = P + np.dot(np.dot(Uf.transpose(), R_blk), Uf);
     P = P + np.eye(T-Tini-N+1) * lambda_g[0, 0];
     P = P + np.dot(lambda_y[0, 0] * Yp.transpose(), Yp);
-    #print(yini_col);
     q = np.dot(-1 * lambda_y[0, 0] * Yp.transpose(), yini_col);
 
     A =  np.vstack((Up, Ep, Ef));
@@ -131,8 +125,6 @@ def qp_DeeP_LCC(*args):
     if constraint_bool: # there exists input/output constraints
         Sf = np.hstack((np.zeros((m, p - m)), np.eye(m)));
         Sf_blk = Sf;
-        #print(np.block([[Sf_blk, np.zeros((Sf.shape[0], Sf_blk.shape[1]))], [np.zeros((Sf_blk.shape[0], Sf.shape[1])), Sf]]))
-        #print(np.block([[Sf_blk, np.zeros((Sf_blk.shape[0], Sf.shape[1]))], [np.zeros((Sf.shape[0], Sf_blk.shape[1])), Sf]]))
         for i in range(2, N+1):
             Sf_blk = np.block([[Sf_blk, np.zeros((Sf_blk.shape[0], Sf.shape[1]))], [np.zeros((Sf.shape[0], Sf_blk.shape[1])), Sf]]);
         G = np.vstack((Uf, -Uf, np.dot(Sf_blk, Yf), np.dot(-Sf_blk, Yf)));
@@ -140,16 +132,7 @@ def qp_DeeP_LCC(*args):
     else: 
         G = [];
         h = [];
-    
-    #wrong way of converting
-    #P = P.astype(np.float64);
-    #q = q.astype(np.float64);
-    #G = G.astype(np.float64);
-    #h = h.astype(np.float64);
-    #A = A.astype(np.float64);
-    #b = b.astype(np.float64);
 
-    print(q.shape);
     P = matrix(P, tc = 'd');
     q = matrix(q, tc = 'd');
     G = matrix(G, tc = 'd');
@@ -159,8 +142,6 @@ def qp_DeeP_LCC(*args):
     
     sol = solvers.qp(P, q, G, h, A, b);
 
-    # u_opt = sol['x']; FIXME
-    # y_opt = sol['primal objective']; FIXME
     u_opt = np.dot(Uf, sol['x']);
     y_opt = np.dot(Yf, sol['x']);
     exitflag = sol['status'];
@@ -178,19 +159,5 @@ def qp_DeeP_LCC(*args):
         problem_status = -2;
     else: # exitflag == 'dual infeasible'
         problem_status = -3;
-    
-    # Convert the array to a pandas DataFrame
-    df = pd.DataFrame(sol['x'])
 
-    # Specify the filename for the Excel file
-    filename = 'sol.xlsx'
-
-    # Write the DataFrame to the Excel file
-    df.to_excel(filename, index=False)
-
-    #print(sol['x']);
-    #print(y_opt);
-    #print(exitflag);
     return u_opt,y_opt,problem_status;
-
-qp_DeeP_LCC(Up,Yp,Uf,Yf,Ep,Ef,uini,yini,eini,Q,R,r,lambda_g,lambda_y,u_limit,s_limit)
